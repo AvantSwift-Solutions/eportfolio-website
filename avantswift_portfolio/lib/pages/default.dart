53a3b151ae8a4eed07a18f1f7de98e5df5dafee9
adasgupta <amritesh.dasgupta@student.unimelb.edu.au>
2023-08-03 13:26:11 +1000

    navbar sample

---
+++ avantswift_portfolio/lib/pages/default.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+class DefaultPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Website'),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to my website',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
---
+++ avantswift_portfolio/lib/pages/defaultNew.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:avantswift_portfolio/pages/default.dart';
+import 'package:flutter/material.dart';
+
+class DefaultPageNew extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Website 2'),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to my website',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
