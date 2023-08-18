771fbe974ec6753ad32a485f88d5610b5bfcc861
Vincent Luu <vinnyluu.02@gmail.com>
2023-08-18 16:28:24 +1000

    Add dummy test

---
+++ test/dummy_test.dart
@@ -0,0 +1,10 @@
+import 'package:test/test.dart';
+
+void main() {
+  group('Dummy Test', () {
+    test('Always Passes', () {
+      // This test always passes
+      expect(1, equals(1));
+    });
+  });
+}
