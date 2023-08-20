ef330142e4a5551d79da491836878053342e727e
KhaiTan6 <khaisyuentan@hotmail.com>
2023-08-20 14:56:51 +0800

    cd push

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -23,8 +23,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e630f5422253b5bc285727a040524244",
-"/": "e630f5422253b5bc285727a040524244",
+"index.html": "c107cc3ec7bc134da718c142f53d4b03",
+"/": "c107cc3ec7bc134da718c142f53d4b03",
 "main.dart.js": "6f56703260a6659d86d710326a5542d9",
 "manifest.json": "5ebec44188ea6754a75aea9454ca2723",
 "version.json": "88a7d7aed18e64fed5a8fe9a24e9ac71"};
