ccd7e25f7dfc7b081723af6345c28485ec6ab7f2
adasgupta <amritesh.dasgupta@student.unimelb.edu.au>
2023-08-25 17:01:33 +1000

    fixing UI components

--- lib/view_pages/landing_page.dart
+++ lib/view_pages/landing_page.dart
@@ -18,48 +18,48 @@ class LandingPage extends StatelessWidget {
           }
 
           final landingPageData = snapshot.data;
+          final screenWidth = MediaQuery.of(context).size.width;
+
+          double titleFontSize = screenWidth * 0.05;
+          double descriptionFontSize = screenWidth * 0.01;
+          double textImagePadding =
+              screenWidth * 0.15; // Adjust the factor as needed
 
           return SingleChildScrollView(
             child: Center(
               child: Padding(
-                padding: const EdgeInsets.all(20.0),
+                padding: const EdgeInsets.all(50.0),
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    // Constrained width text boxes
-                    Container(
-                      width: 500, // Adjust this value as needed
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            landingPageData!.landingPageTitle,
-                            style: const TextStyle(
-                              fontSize: 56, // Increased font size
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          const SizedBox(height: 20), // Increased spacing
-                          Text(
-                            landingPageData.landingPageDescription,
-                            style: TextStyle(fontSize: 14),
-                          ),
-                          const SizedBox(height: 20),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(width: 20), // Adding some horizontal spacing
-                    // Image on the right
                     Expanded(
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: Image.network(
-                          landingPageData.imageURL,
-                          width: 200,
-                          height: 200,
-                          fit: BoxFit.cover,
+                      child: Container(
+                        padding: EdgeInsets.only(right: textImagePadding),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              landingPageData!.landingPageTitle,
+                              style: TextStyle(
+                                fontSize: titleFontSize,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            Text(
+                              landingPageData.landingPageDescription,
+                              style: TextStyle(fontSize: descriptionFontSize),
+                            ),
+                            const SizedBox(height: 20),
+                          ],
                         ),
                       ),
+                    ),
+                    Image.network(
+                      landingPageData.imageURL,
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      height: 500,
+                      fit: BoxFit.cover,
                     ),
                   ],
                 ),
