fee0e62bf1f886b5e5d2ee184576afe22e6bcdfb
Vincent Luu <vinnyluu.02@gmail.com>
2023-09-06 19:57:10 +1000

    Start implementing unit tests

---
+++ test/models/iskill_model_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:avantswift_portfolio/models/ISkill.dart';
+
+void main() {
+  group('Technical Skill class tests', () {
+    // Could not test fromDocumentSnapshot because it uses a
+    // DocumentSnapshot which is a final class and cannot be extended?
+
+    test('ISkill.toMap should convert iskill to a map', () {
+      final iskill = ISkill(
+        isid: 'iskill123',
+        name: 'mockISkill',
+      );
+      final iskillMap = iskill.toMap();
+
+      expect(iskillMap['tsid'], 'iskill123');
+      expect(iskillMap['name'], 'mockISkill');
+    });
+
+  });
+}
---
+++ test/models/tskill_model_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:avantswift_portfolio/models/TSkill.dart';
+
+void main() {
+  group('Technical Skill class tests', () {
+    // Could not test fromDocumentSnapshot because it uses a
+    // DocumentSnapshot which is a final class and cannot be extended?
+
+    test('TSkill.toMap should convert tskill to a map', () {
+      final tskill = TSkill(
+        tsid: 'tskill123',
+        name: 'mockTSkill',
+        imageURL: 'https://example.com/image.jpg'
+      );
+      final tskillMap = tskill.toMap();
+
+      expect(tskillMap['tsid'], 'tskill123');
+      expect(tskillMap['name'], 'mockTSkill');
+      expect(tskillMap['imageURL'], 'https://example.com/image.jpg');
+    });
+
+  });
+}
