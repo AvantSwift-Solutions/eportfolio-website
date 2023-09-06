89fc947eae4f7b1fdb9f9d68d35ceb6f17383d0b
ashzhang1 <ashzhang1@gmail.com>
2023-09-07 00:37:25 +1000

    created repo service for reccomendations

---
+++ lib/reposervice/reccomendation_repo_services.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:developer';
+import '../models/Reccomendation.dart';
+
+class ProfessionalExperienceRepoService {
+  Future<List<Reccomendation>?> getAllReccomendations() async {
+    try {
+      QuerySnapshot snapshot =
+          await FirebaseFirestore.instance.collection('Reccomendation').get();
+      final tmp = snapshot.docs
+          .map((doc) => Reccomendation.fromDocumentSnapshot(doc))
+          .toList();  
+      return tmp;
+    } catch (e) {
+      log('error: $e');  
+      return null;
+    }
+  }
+}
