075518920b0244041a7a10864f53708277f6a53f
ashzhang1 <ashzhang1@gmail.com>
2023-09-07 11:51:20 +1000

    fixed unit testing for reccomendations

--- test/controller/reccomendation_section_admin_controller_test.dart
+++ test/controller/reccomendation_section_admin_controller_test.dart
@@ -1,5 +1,6 @@
 import 'package:avantswift_portfolio/models/Reccomendation.dart';
 import 'package:avantswift_portfolio/reposervice/reccomendation_repo_services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -18,18 +19,18 @@ void main() {
 
   setUp(() {
     mockReccomendation1 = MockReccomendation();
-    when(mockReccomendation1.rid).thenReturn('rid123');
-    when(mockReccomendation1.colleagueName).thenReturn('Steve Jobs');
-    when(mockReccomendation1.colleagueJobTitle).thenReturn('CEO');
-    when(mockReccomendation1.description).thenReturn('This is a reccomendation');
+    when(mockReccomendation1.rid).thenReturn('mockrid1');
+    when(mockReccomendation1.colleagueName).thenReturn('Mock Name1');
+    when(mockReccomendation1.colleagueJobTitle).thenReturn('Mock Job Title1');
+    when(mockReccomendation1.description).thenReturn('Mock Location1');
     when(mockReccomendation1.imageURL).thenReturn('http://example.com/mock_image1.jpg');
 
     mockReccomendation2 = MockReccomendation();
-    when(mockReccomendation2.rid).thenReturn('rid321');
-    when(mockReccomendation2.colleagueName).thenReturn('ColleagueName2');
-    when(mockReccomendation2.colleagueJobTitle).thenReturn('ColleagueJobTitle2');
-    when(mockReccomendation2.description).thenReturn('This is a reccomendation2');
-    when(mockReccomendation2.imageURL).thenReturn('http://example.com/mock_image2.jpg');
+    when(mockReccomendation2.rid).thenReturn('mockrid2');
+    when(mockReccomendation2.colleagueName).thenReturn('Mock Name2');
+    when(mockReccomendation2.colleagueJobTitle).thenReturn('Mock Job Title2');
+    when(mockReccomendation2.description).thenReturn('Mock Location1');
+    when(mockReccomendation2.imageURL).thenReturn('http://example.com/mock_image1.jpg');
     
     mockRepoService = MockReccomendationRepoService();
     controller = ReccomendationSectionAdminController(mockRepoService);
@@ -40,25 +41,25 @@ void main() {
 
     when(mockRepoService.getAllReccomendations()).thenAnswer((_) async
       => [mockReccomendation1, mockReccomendation2]);
-    final allReccomendations = await controller.getReccomendationSectionData();
+    final getAllReccomendations = await controller.getReccomendationSectionData();
 
     // Make assertions on the returned data
-    expect(allReccomendations!.length, 2);
+    expect(getAllReccomendations!.length, 2);
 
-    var recc1 = allReccomendations[0];
-    var recc2 = allReccomendations[1];
+    var rec1 = getAllReccomendations[0];
+    var rec2 = getAllReccomendations[1];
 
-    expect(recc1.rid, mockReccomendation1.rid);
-    expect(recc1.colleagueName, mockReccomendation1.colleagueName);
-    expect(recc1.colleagueJobTitle, mockReccomendation1.colleagueJobTitle);
-    expect(recc1.description, mockReccomendation1.description);
-    expect(recc1.imageURL, mockReccomendation1.imageURL);
+    expect(rec1.rid, mockReccomendation1.rid);
+    expect(rec1.colleagueName, mockReccomendation1.colleagueName);
+    expect(rec1.colleagueJobTitle, mockReccomendation1.colleagueJobTitle);
+    expect(rec1.description, mockReccomendation1.description);
+    expect(rec1.imageURL, mockReccomendation1.imageURL);
 
-    expect(recc2.rid, mockReccomendation2.rid);
-    expect(recc2.colleagueName, mockReccomendation2.colleagueName);
-    expect(recc2.colleagueJobTitle, mockReccomendation2.colleagueJobTitle);
-    expect(recc2.description, mockReccomendation2.description);
-    expect(recc2.imageURL, mockReccomendation2.imageURL);
+    expect(rec2.rid, mockReccomendation2.rid);
+    expect(rec2.colleagueName, mockReccomendation2.colleagueName);
+    expect(rec2.colleagueJobTitle, mockReccomendation2.colleagueJobTitle);
+    expect(rec2.description, mockReccomendation2.description);
+    expect(rec2.imageURL, mockReccomendation2.imageURL);
 
   });
 
@@ -74,24 +75,24 @@ void main() {
     expect(reccomendationSectionData, null);
   });
 
-  test('getReccomendationSectionData returns true on successful update', () async {
+  test('updateReccomendationSectionData returns true on successful update', () async {
     when(mockRepoService.getAllReccomendations()).thenAnswer((_) async
       => [mockReccomendation1, mockReccomendation2]);
     when(mockReccomendation1.update()).thenAnswer((_) async => true);
 
     final updateResult = await controller.updateReccomendationSectionData(0,
       Reccomendation(
-        rid: 'mockReccId1',
-        colleagueName: 'New Mock Colleague Name1',
-        colleagueJobTitle: 'New Mock Colleague Job Title1',
+        rid: 'rid1',
+        colleagueName: 'New Mock Name1',
+        colleagueJobTitle: 'New Mock Job Title1',
         description: 'New Mock Description1',
         imageURL: 'http://example.com/new_mock_image1.jpg',
       ),
     );
 
     expect(updateResult, true);
-    verify(mockReccomendation1.colleagueName = 'New Mock Colleague Name1');
-    verify(mockReccomendation1.colleagueJobTitle = 'New Mock Colleague Job Title1');
+    verify(mockReccomendation1.colleagueName = 'New Mock Name1');
+    verify(mockReccomendation1.colleagueJobTitle = 'New Mock Job Title1');
     verify(mockReccomendation1.description = 'New Mock Description1');
     verify(mockReccomendation1.imageURL = 'http://example.com/new_mock_image1.jpg');
 
@@ -103,9 +104,9 @@ void main() {
 
     final updateResult = await controller.updateReccomendationSectionData(0,
       Reccomendation(
-        rid: 'mockReccId1',
-        colleagueName: 'New Mock Colleague Name1',
-        colleagueJobTitle: 'New Mock Colleague Job Title1',
+        rid: 'rid1',
+        colleagueName: 'New Mock Name1',
+        colleagueJobTitle: 'New Mock Job Title1',
         description: 'New Mock Description1',
         imageURL: 'http://example.com/new_mock_image1.jpg',
       ),
