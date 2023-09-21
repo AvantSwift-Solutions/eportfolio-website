8a0d748b5a507fd9cfe7bd401935cb7d4b703441
Kaobara <dannymahmud1@gmail.com>
2023-09-21 21:01:33 +1000

    modified constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -11,4 +11,15 @@ class Constants {
       'https://firebasestorage.googleapis.com/v0/b/avantswiftsolutions.appspot.com/o/images%2Fmail.svg?alt=media&token=716cdb7d-574e-4522-af92-e71bdc31ed6e';
   static const linkedinSVGURL =
       'https://firebasestorage.googleapis.com/v0/b/avantswiftsolutions.appspot.com/o/images%2Flinkedin.svg?alt=media&token=97129468-ac7c-445a-a0cd-42c6f1031fca';
+      
+  static const double kScreenWidthDivider = 0.055;
+  static const double kScreenDividerThickness = 2.0;
+  static const double kLogoSize = 0.048;
+  static const double kVerticalSpacing = 0.015;
+  static const double kHorizontalSpacing = 5;
+  static const double educationWidgetHeight = 400;
+  static const double navigationCircleSize = 10;
+  static const double greyOpacity = 0.5;
+  static const int pageAnimationDuration = 300;
+  static const double kTitleDividerLength = 0.319;
 }
