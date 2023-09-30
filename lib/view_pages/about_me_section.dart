e0184d06889b82fc68cf889782a3860ce8bb66fb
KhaiTan6 <khaisyuentan@hotmail.com>
2023-09-30 15:10:43 +0800

    implemented responsiveness for aboutme section

--- lib/view_pages/about_me_section.dart
+++ lib/view_pages/about_me_section.dart
@@ -1,3 +1,5 @@
+import 'package:avantswift_portfolio/dto/about_me_section_dto.dart';
+import 'package:avantswift_portfolio/models/User.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import '../constants.dart';
@@ -6,7 +8,7 @@ import '../reposervice/user_repo_services.dart';
 import '../ui/custom_texts/public_view_text_styles.dart';
 
 class AboutMeSection extends StatefulWidget {
-  const AboutMeSection({super.key});
+  const AboutMeSection({Key? key}) : super(key: key);
 
   @override
   AboutMeSectionState createState() => AboutMeSectionState();
@@ -15,6 +17,31 @@ class AboutMeSection extends StatefulWidget {
 class AboutMeSectionState extends State<AboutMeSection> {
   final AboutMeSectionController _aboutMeSectionController =
       AboutMeSectionController(UserRepoService());
+
+  late AboutMeSectionDTO aboutMeData;
+  late String _imageURL;
+  bool _dataLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    aboutMeData = (await _aboutMeSectionController.getAboutMeSectionData())!;
+    if (aboutMeData.imageURL?.isEmpty ?? false) {
+      final replacementURL = await getReplacementURL();
+      if (replacementURL.isNotEmpty) {
+        aboutMeData.imageURL = replacementURL;
+      }
+    }
+
+    setState(() {
+      _imageURL = aboutMeData.imageURL ?? Constants.replaceImageURL;
+      _dataLoaded = true;
+    });
+  }
 
   Future<String> getReplacementURL() async {
     final storage = FirebaseStorage.instance;
@@ -25,82 +52,104 @@ class AboutMeSectionState extends State<AboutMeSection> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _aboutMeSectionController.getAboutMeSectionData(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return const Center(child: Text('Error loading data'));
-        }
-        if (snapshot.data?.imageURL?.isEmpty ?? false) {
-          // If imageURL is empty, fetch the replacement URL and update it
-          getReplacementURL().then((replacementURL) {
-            if (replacementURL.isNotEmpty) {
-              snapshot.data?.imageURL = replacementURL;
-            }
-          });
-        }
-        final aboutMeSectionData = snapshot.data;
-
-        final screenWidth = MediaQuery.of(context).size.width;
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        final screenWidth = constraints.maxWidth;
 
         double titleFontSize = screenWidth * 0.05;
         double descriptionFontSize = screenWidth * 0.01;
-        double spacing = screenWidth * 0.15;
-        return Center(
-          child: Padding(
-            padding: const EdgeInsets.all(50.0),
-            child: Row(
-              // Use Row for side-by-side layout
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  flex: 2, // Adjust flex as needed
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'A Bit About Myself...',
-                        style: PublicViewTextStyles.generalHeading.copyWith(
+        double imageSize = screenWidth > 600 ? screenWidth * 0.4 : screenWidth * 0.8;
+
+        // Check if it's a wide screen (desktop/tablet) or not (mobile)
+        if (screenWidth > 600) {
+          return Center(
+            child: Padding(
+              padding: EdgeInsets.all(screenWidth * 0.1),
+              child: Flex(
+                direction: Axis.horizontal,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'A Bit About Myself...',
+                          style: PublicViewTextStyles.generalHeading.copyWith(
                             fontSize: titleFontSize * 0.8,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 20),
-                      Padding(
-                        padding: EdgeInsets.only(right: spacing),
-                        child: Text(
-                          aboutMeSectionData?.aboutMe ?? 'Default Description',
-                          style: PublicViewTextStyles.generalBodyText.copyWith(
-                            fontSize: descriptionFontSize * 1.3,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 20),
-                    ],
+                        const SizedBox(height: 20),
+                        Padding(
+                          padding: EdgeInsets.only(right: screenWidth * 0.15),
+                          child: Text(
+                            _dataLoaded ? (aboutMeData.aboutMe ?? 'Default Description') : 'Loading...',
+                            style: PublicViewTextStyles.generalBodyText.copyWith(
+                              fontSize: descriptionFontSize * 2,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                      ],
+                    ),
                   ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 100.0), // Adjust padding as needed
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 0),
+                      child: Image.network(
+                        _dataLoaded ? _imageURL : Constants.replaceImageURL,
+                        width: imageSize,
+                        height: imageSize,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return Center(
+            child: Padding(
+              padding: EdgeInsets.all(screenWidth * 0.1),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'A Bit About Myself...',
+                    style: PublicViewTextStyles.generalHeading.copyWith(
+                      fontSize: titleFontSize * 0.8,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: EdgeInsets.only(right: screenWidth * 0.15),
+                    child: Text(
+                      _dataLoaded ? (aboutMeData.aboutMe ?? 'Default Description') : 'Loading...',
+                      style: PublicViewTextStyles.generalBodyText.copyWith(
+                        fontSize: descriptionFontSize * 2,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 0),
                     child: Image.network(
-                      aboutMeSectionData != null &&
-                              aboutMeSectionData.imageURL != "" &&
-                              aboutMeSectionData.imageURL!.isNotEmpty
-                          ? aboutMeSectionData.imageURL!
-                          : Constants.replaceImageURL,
-                      width: 200,
-                      height: 400,
+                      _dataLoaded ? _imageURL : Constants.replaceImageURL,
+                      width: imageSize,
+                      height: imageSize,
                       fit: BoxFit.cover,
                     ),
                   ),
-                )
-              ],
+                ],
+              ),
             ),
-          ),
-        );
+          );
+        }
       },
     );
   }
